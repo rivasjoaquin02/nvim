8c08ab0ca4ff4e383f1c19f2d87fcb579cb02598
Strange <rivasjoaquin02@gmail.com>
2024-01-02 22:51:18 -0500

    updated lsp file

--- lua/strange/plugins/lsp.lua
+++ lua/strange/plugins/lsp.lua
@@ -1,43 +1,48 @@
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		config = function()
-			local lsp = require("lsp-zero")
-			lsp.extend_lspconfig()
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        config = function()
+            local lsp = require("lsp-zero")
+            lsp.extend_lspconfig()
 
-			lsp.on_attach(function(_, bufnr)
-				local opts = { buffer = bufnr, remap = false }
+            lsp.on_attach(function(_, bufnr)
+                local opts = { buffer = bufnr, remap = false }
 
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-				vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-				vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-				vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-				vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-			end)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+                vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+            end)
 
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "tsserver", "pyright" },
-				handlers = {
-					lsp.default_setup,
-					lua_ls = function()
-						local lua_opts = lsp.nvim_lua_ls()
-						require("lspconfig").lua_ls.setup(lua_opts)
-					end,
-				},
-			})
-		end,
-	},
-	{ "neovim/nvim-lspconfig" },
-	-- lsp server manager
-	{ "williamboman/mason.nvim" },
-	{ "williamboman/mason-lspconfig.nvim" },
-	-- UI
-	{ "j-hui/fidget.nvim", config = true, event = "VeryLazy" },
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "clangd", "tsserver", "pyright" },
+                handlers = {
+                    lsp.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp.nvim_lua_ls()
+                        require("lspconfig").lua_ls.setup(lua_opts)
+                    end,
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("lspconfig").tailwindcss.setup({})
+        end,
+    },
+    -- lsp server manager
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    -- UI
+    { "j-hui/fidget.nvim",                config = true, event = "VeryLazy" },
 }
