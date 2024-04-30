1b80994f3821ecc36f0eeb21220f6ee83c683219
Strange <rivasjoaquin02@gmail.com>
2024-04-30 15:58:34 -0400

    refactor(format): replacement of none-ls to conform

--- lua/custom/plugins/autoformat.lua
+++ lua/custom/plugins/autoformat.lua
@@ -1,4 +1,5 @@
-return { -- Autoformat
+-- Autoformat
+return {
   'stevearc/conform.nvim',
   lazy = false,
   keys = {
@@ -12,25 +13,37 @@ return { -- Autoformat
     },
   },
   opts = {
-    notify_on_error = false,
+    log_level = vim.log.levels.ERROR,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
+      -- local disable_filetypes = { c = true, cpp = true }
+      local disable_filetypes = {}
       return {
         timeout_ms = 500,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
+    -- Conform can also run multiple formatters sequentially
     formatters_by_ft = {
+      -- Use the "*" filetype to run formatters on all filetypes.
+      ['*'] = { 'codespell' },
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
+      python = function(bufnr)
+        if require('conform').get_formatter_info('ruff_format', bufnr).available then
+          return { 'ruff_format' }
+        else
+          return { 'isort', 'black' }
+        end
+      end,
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      javascript = { { 'prettierd', 'prettier' } },
+      typescript = { { 'prettierd', 'prettier' } },
+      c = { { 'clang_format' } },
+      cpp = { { 'clang_format' } },
     },
   },
 }
