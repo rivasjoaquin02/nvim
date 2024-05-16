0991e17effa238994cf61dfd847bf259d2cda649
Strange <rivasjoaquin02@gmail.com>
2024-05-15 20:45:37 -0400

    refactor: fix completion plugin + add db plugin

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -1,21 +1,27 @@
 -- dadbod.vim: Modern database interface for Vim
+-- return {
+--     'kristijanhusak/vim-dadbod-ui',
+--     enabled = false,
+--     lazy = true,
+--     event = 'VeryLazy',
+--     dependencies = {
+--         { 'tpope/vim-dadbod' },
+--         { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' } },
+--     },
+--     cmd = {
+--         'DBUI',
+--         'DBUIToggle',
+--         'DBUIAddConnection',
+--         'DBUIFindBuffer',
+--     },
+--     init = function()
+--         -- Your DBUI configuration
+--         vim.g.db_ui_use_nerd_fonts = 1
+--     end,
+-- }
+
 return {
-  'kristijanhusak/vim-dadbod-ui',
-  enabled = false,
-  lazy = true,
-  event = 'VeryLazy',
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
+    'tpope/vim-dadbod',
+    'kristijanhusak/vim-dadbod-completion',
+    'kristijanhusak/vim-dadbod-ui',
 }
