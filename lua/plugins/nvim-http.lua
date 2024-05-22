a18a10a661d2d9b978890ac5479d265dfaf7a474
Strange <rivasjoaquin02@gmail.com>
2024-05-22 17:10:16 -0400

    removed unused files

---
+++ lua/plugins/nvim-http.lua
@@ -0,0 +1,11 @@
+-- You can use this plugin to run HTTP requests directly in your favourite text editor.
+-- install python-pynvim python-requests
+
+return {
+    'BlackLight/nvim-http',
+    lazy = true,
+    keys = {
+        { '<leader>rr', '<Cmd>Http<CR>', desc = '[R]un Http Request' },
+        { '<leader>rs', '<Cmd>HttpStop<CR>', desc = 'Stop Http Request' },
+    },
+}
